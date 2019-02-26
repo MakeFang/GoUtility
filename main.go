f97c11a807b3c2c998a47248202b64ed805c694c
Kuan-Ying Fang <kuanyingfang@gmail.com>
2019-02-26 09:09:12 -0800

    implementing command parsing

--- main.go
+++ main.go
@@ -9,6 +9,35 @@ import (
     _ "github.com/joho/godotenv/autoload"
 )
 
+func FormatCommands(input string) []string {
+    result := strings.Split(input, " ")
+    // fmt.Println(result)
+    return result
+}
+
+// controller
+func ControllerRouting(args []string) string {
+    numArgs := len(args)
+    if numArgs > 3 || numArgs < 1 {
+        return "size bad"
+    }
+    switch firstArg := args[0]; firstArg {
+    case "get":
+        return "get"
+    case "set":
+        return "set"
+    default:
+        return "help"
+    }
+    // for _, arg := range args {
+    //
+    // }
+}
+
+// schema
+
+// Private logic
+
 func main() {
     api := slack.New(os.Getenv("BOT_OAUTH_ACCESS_TOKEN"))
     // logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
@@ -28,8 +57,11 @@ func main() {
                 fmt.Println("message not direct: ignored.")
                 continue
             }
-            fmt.Printf("%+v\n", rtm.NewOutgoingMessage("hello", ev.Msg.Channel))
-            rtm.SendMessage(rtm.NewOutgoingMessage("hello", ev.Msg.Channel))
+            // fmt.Printf("%+v\n", rtm.NewOutgoingMessage("hello", ev.Msg.Channel))
+            // fmt.Println(FormatCommands(ev.Msg.Text))
+            formattedMsg := FormatCommands(ev.Msg.Text)
+            outputMsg := ControllerRouting(formattedMsg)
+            rtm.SendMessage(rtm.NewOutgoingMessage(outputMsg, ev.Msg.Channel))
         }
     }
 }

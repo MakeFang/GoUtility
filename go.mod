e5625781d3567368593402622f5822686d16ec1f
Kuan-Ying Fang <kuanyingfang@gmail.com>
2019-02-26 16:18:31 -0800

    [ugly] implementing database create and query. Refactor next

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/MakeFang/GoUtility
 
 require (
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/jinzhu/gorm v1.9.2 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/nlopes/slack v0.5.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "strings"
     "time"
     "github.com/nlopes/slack"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/sqlite"
     _ "github.com/joho/godotenv/autoload"
 )
 
@@ -17,24 +19,45 @@ func FormatCommands(input string) []string {
 }
 
 // controller
-func ControllerRouting(args []string) string {
+func ControllerRouting(args []string, db *gorm.DB, userID string) string {
     numArgs := len(args)
     if numArgs > 3 || numArgs < 1 {
         return "help"
     }
     switch firstArg := args[0]; firstArg {
     case "get":
-        response, err := getParsing(args[1:])
+        response, err := GetParsing(args[1:])
         if err != nil {
             log.Fatal(err)
         }
+        var reservations []Reservation
+        db.Find(&reservations)
+        response = fmt.Sprintf("%v", reservations)
         return response
     case "set":
-        response, err := setParsing(args[1:])
+        response, err := SetParsing(args[1:])
         if err != nil {
             log.Fatal(err)
         }
-        return response
+        t1, e := time.Parse(time.RFC3339, response)
+        if e != nil {
+            return "help"
+        }
+        var reservation Reservation
+        var resMsg string
+        newRes := Reservation{StartTime: t1, UserSlackID: userID, RoomID: "1"}
+        db.Where(Reservation{StartTime: t1, UserSlackID: "", RoomID: "1"}).First(&reservation)
+        fmt.Println("look up reservations", reservation)
+        if reservation == (Reservation{}) {
+            fmt.Println("no records")
+            db.Create(&newRes)
+            // db.Where("StartTime = ?", t1).First(&reservation)
+            // fmt.Println(reservation)
+            resMsg = "Success. Reserved " + t1.Format(time.RFC3339)
+        } else {
+            resMsg = "Failure. Time slot taken already."
+        }
+        return resMsg
     default:
         return "help"
     }
@@ -43,7 +66,7 @@ func ControllerRouting(args []string) string {
     // }
 }
 
-func getParsing(args []string) (string, error) {
+func GetParsing(args []string) (string, error) {
     numArgs := len(args)
     if numArgs != 1 {
         return "help", nil
@@ -53,12 +76,12 @@ func getParsing(args []string) (string, error) {
         return "help", e
     }
     startTime := t1.Truncate(30*time.Minute)
-    return startTime.String(), nil
+    return startTime.Format(time.RFC3339), nil
 }
 
-func setParsing(args []string) (string, error) {
+func SetParsing(args []string) (string, error) {
     numArgs := len(args)
-    if numArgs != 2 {
+    if numArgs != 1 {
         return "help", nil
     }
     t1, e := time.Parse(time.RFC3339, args[0])
@@ -66,14 +89,34 @@ func setParsing(args []string) (string, error) {
         return "help", e
     }
     startTime := t1.Truncate(30*time.Minute)
-    return startTime.String(), nil
+    return startTime.Format(time.RFC3339), nil
 }
 
 // schema
+type Reservation struct {
+    gorm.Model
+    StartTime time.Time
+    UserSlackID string
+    RoomID string
+}
+
+type User struct {
+    gorm.Model
+    SlackID string
+    Reservations []Reservation `gorm:"foreignkey:UserSlackID;association_foreignkey:SlackID"`
+}
 
 // Private logic
 
 func main() {
+    db, err := gorm.Open("sqlite3", "test.db")
+    if err != nil {
+        panic("failed to connect to database")
+    }
+    defer db.Close()
+
+    db.AutoMigrate(&Reservation{}, &User{})
+
     api := slack.New(os.Getenv("BOT_OAUTH_ACCESS_TOKEN"))
     // logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
     // slack.SetLogger(logger)
@@ -92,10 +135,20 @@ func main() {
                 fmt.Println("message not direct: ignored.")
                 continue
             }
+            var user User
+            var allUsers []User
+            // fmt.Println(ev.Msg.User)
+            // fmt.Printf("%+v\n", User{SlackID: ev.Msg.User})
+            db.Where(User{SlackID: ev.Msg.User}).FirstOrCreate(&user)
+            db.Find(&allUsers)
+            fmt.Println(allUsers)
+            fmt.Println(user)
+            // db.Where("SlackID = ?", ev.Msg.User).First(&user)
+            // fmt.Printf("%+v\n", user)
             // fmt.Printf("%+v\n", rtm.NewOutgoingMessage("hello", ev.Msg.Channel))
             // fmt.Println(FormatCommands(ev.Msg.Text))
             formattedMsg := FormatCommands(ev.Msg.Text)
-            outputMsg := ControllerRouting(formattedMsg)
+            outputMsg := ControllerRouting(formattedMsg, db, ev.Msg.User)
             rtm.SendMessage(rtm.NewOutgoingMessage(outputMsg, ev.Msg.Channel))
         }
     }
